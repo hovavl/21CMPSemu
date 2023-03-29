0aaa53b2e10fd9b4cc0ecb61bedbda3404bff5cd
Hovav <h_lazare@apple.com>
2023-03-29 19:52:01 +0300

    add the NN files

---
+++ ps_model_test.py
@@ -0,0 +1,73 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import pickle
+from NN_emulator import emulator
+import random
+
+x=1
+with open('/Users/hovavlazare/PycharmProjects/global_signal_model/log_ps_model_files/training_files.pk', 'rb') as f:
+    training_params, features, val_params, val_features, testing_params, testing_features, model_params, k_range = pickle.load(
+        f)
+
+myEmulator = emulator(restore=True, use_log=False,
+                      files_dir='/Users/hovavlazare/PycharmProjects/global_signal_model/log_ps_model_files',
+                      name='small_data_set_emulator')
+
+print(myEmulator.NN.summary())
+
+ind = testing_params['L_X'] > 38
+filter_test_params = {}
+for key in testing_params.keys():
+    filter_test_params[key] = testing_params[key][ind]
+filter_test_features = testing_features[ind, :]
+
+test_loss, pred = myEmulator.test_MRE(filter_test_params, filter_test_features)
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(30, 8))
+
+ax[0].boxplot(test_loss, whis=(5, 95), whiskerprops={'ls': 'dotted', 'linewidth': 1, 'color': 'b'},
+              medianprops={'color': 'r', 'linewidth': 0.5}, showfliers=True)
+worst_ind = np.argmax(test_loss)
+
+# worst_params = {}
+# for key in filter_test_params.keys():
+#     worst_params[key] = [filter_test_params[key][worst_ind]]
+
+worst_pred = pred[worst_ind, :]
+worst_feature = filter_test_features[worst_ind, :]
+print(np.median(test_loss))
+# print(worst_params)
+
+# count = 0
+# for i in range(pred.shape[0]):
+#     sig = pred[i,:]
+#     if np.any(sig < 0):
+#         count+=1
+# print('count: ',count)
+
+ax[1].plot(k_range, worst_feature, c='r', label='Original')
+ax[1].plot(k_range, worst_pred, c='b', label='NN reconstructed', linestyle='--')
+ax[1].set_xscale('log')
+ax[1].set_xlabel('$k\,[1/\mathrm{Mpc}]$', fontsize=20)
+ax[1].set_ylabel('$\Delta_{21}^2\,[\mathrm{mK}^2]\,\,(z=$' + str(7.9) + ')', fontsize=15)
+ax[1].legend()
+
+plt.show()
+
+fig, ax = plt.subplots(nrows=1, ncols=5, figsize=(50, 10))
+for i in range(5):
+    n = random.randint(0, len(filter_test_params['F_STAR10']) - 1)
+    pred_n = pred[n]
+
+    true = filter_test_features[n]
+    ax[i].semilogx(k_range, true, 'blue', label='Original')
+    ax[i].semilogx(k_range, pred_n, 'red', label='NN reconstructed', linestyle='--')
+    ax[i].set_xlabel('$k\,[1/\mathrm{Mpc}]$', fontsize=20)
+    ax[i].set_ylabel('$\Delta_{21}^2\,[\mathrm{mK}^2]\,\,(z=$' + str(7.9) + ')', fontsize=15)
+    ax[i].legend(fontsize=15)
+    ax[i].grid()
+
+plt.show()
+
+
+x=1
