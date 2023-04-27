2066f309429560ff0ee21489e5dd7aba9f8938bf
Hovav <h_lazare@apple.com>
2023-04-27 15:42:12 +0300

    second part of last commit

---
+++ MCMC_code/UV_LF.py
@@ -0,0 +1,56 @@
+import json
+import py21cmfast as p21c
+import copy
+import numpy as np
+import matplotlib.pyplot as plt
+
+# read UV luminosity function data
+with open('/gpfs0/elyk/users/hovavl/jobs/21cm_mcmc_job/UV_LU_data_reduced_new.json', 'r') as openfile:
+    # Reading from json file
+    UV_LU_data = json.load(openfile)
+
+user_params = {"DIM": 512, "HII_DIM": 128, "BOX_LEN": 256, "N_THREADS": 1, 'USE_RELATIVE_VELOCITIES': False,
+               "POWER_SPECTRUM": 0}
+flag_options = {"USE_MASS_DEPENDENT_ZETA": True, "USE_CMB_HEATING": False, "USE_LYA_HEATING": False,
+                'USE_TS_FLUCT': True,
+                'USE_MINI_HALOS': False, "INHOMO_RECO": True}
+
+
+def predict_luminosity(theta):
+    tmp = copy.deepcopy(theta)
+    F_STAR10, F_STAR7_MIMI, ALPHA_STAR, ALPHA_STAR_MINI, F_ESC10, F_ESC7_MINI, ALPHA_ESC, M_TURN, L_X, NU_X_THRESH, = tmp
+    astro_params = {'F_STAR10': F_STAR10, 'F_ESC10': F_ESC10, 'L_X': L_X, 'M_TURN': M_TURN,
+              'NU_X_THRESH': NU_X_THRESH, 'ALPHA_STAR': ALPHA_STAR, 'ALPHA_ESC': ALPHA_ESC
+              #'F_STAR7_MIMI': [F_STAR7_MIMI], 'ALPHA_STAR_MINI': [ALPHA_STAR_MINI], 'F_ESC7_MINI': [F_ESC7_MINI]
+                    }
+    redshifts = [6, 8, 10]
+    Lf = p21c.compute_luminosity_function(
+        redshifts=redshifts,
+        user_params=user_params,
+        astro_params=astro_params,
+        flag_options=flag_options,
+        nbins=100,
+        component=0
+    )
+    interp_lum = []
+    for i, m_uv in enumerate(Lf[0]):
+        fp = np.array(Lf[2][i])
+        log_fp = np.isnan(fp)
+
+        fp = 10**(np.flip(fp[~log_fp]))
+        m_uv = np.flip(m_uv[~log_fp])
+        x_eval = np.array(UV_LU_data[str(redshifts[i])]['M'])
+        x_eval = x_eval[x_eval <= np.max(m_uv)]
+        y = np.interp(x_eval, m_uv, fp)
+        interp_lum += [y]
+        # plt.plot(x_eval,y, color = 'b', ls = '--', label = 'interpolated')
+        # plt.plot(m_uv[30:52], fp[30:52] , color = 'r', label = 'real')
+        # plt.legend()
+        # plt.savefig('uv_luminosity_function')
+        # break
+    return interp_lum
+
+
+
+
+
