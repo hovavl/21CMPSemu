77aaf937d04941656078522f02cc7a8bd32afee7
hovavl <hovavl@post.bgu.ac.il>
2026-07-11 18:25:11 +0300

    Checkpoint from VS Code for cloud agent session

--- quick_21cmFAST.py
+++ quick_21cmFAST.py
@@ -8,16 +8,24 @@ from tau import calc_tau
 from power_spectrum import powerspectra
 
 
+# params = sys.argv[1:]
+# m_FDM = float(params[0])
+# f_FDM = float(params[1])
 
 user_params = {"DIM": 512, "HII_DIM": 128, "BOX_LEN": 256, "N_THREADS": 1, 'USE_RELATIVE_VELOCITIES': True,
-               'DO_VCB_FIT': True,
-               "POWER_SPECTRUM": 6}
-flag_options = {"USE_MASS_DEPENDENT_ZETA": True, "USE_CMB_HEATING": False, "USE_LYA_HEATING": False,
+               'DO_VCB_FIT': True, 'RUN_CLASS': True, 
+               "POWER_SPECTRUM": 5}
+flag_options = {"USE_MASS_DEPENDENT_ZETA": True, "USE_CMB_HEATING": False, "USE_LYA_HEATING": True,
                 'USE_TS_FLUCT': True,
                 'USE_MINI_HALOS':False, "INHOMO_RECO": True}
 
 cosmo_params = {"hlittle": 0.6736, "OMb": 0.0493, "OMm": 0.3153,
-                "A_s": 2.1e-9, "POWER_INDEX": 0.9649}
+                "A_s": 2.1e-9, "POWER_INDEX": 0.9665}
+                
+#cosmo_params['m_FDM'] = m_FDM
+#cosmo_params['f_FDM'] = f_FDM
+
+# print(cosmo_params)
 
 
 # print(cosmo_params['hlittle']**2 * cosmo_params['OMb'])
@@ -35,9 +43,9 @@ def run(theta):
                                    random_seed=1,
                                    write=False,
                                    user_params=user_params,
-                                   lightcone_quantities={'brightness_temp', 'density', 'xH_box', 'Ts_box', 'dNrec_box',
+                                   lightcone_quantities={'brightness_temp', 'density', 'xH_box', 'Ts_box', 'dNrec_box','Tk_box',
                                                          'Gamma12_box', 'J_21_LW_box'},
-                                   global_quantities={'brightness_temp', 'density', 'xH_box', 'Ts_box', 'dNrec_box',
+                                   global_quantities={'brightness_temp', 'density', 'xH_box', 'Ts_box', 'dNrec_box','Tk_box',
                                                       'Gamma12_box', 'J_21_LW_box'},
                                    flag_options=flag_options,
                                    astro_params=astro_params,
@@ -58,10 +66,11 @@ def run(theta):
               'global_xH': global_xH,
               'tau': tau,
               'T_b':T_b,
-              'z_global': z_global
+              'z_global': z_global,
+              'T_k': lightcone.global_quantities['Tk_box']
               }
 
-    nchunks = 20
+    nchunks = 64
 
     data_PS, z_PS = powerspectra(lightcone_box=lightcone.brightness_temp,
                                  BOX_LEN=lightcone.user_params.BOX_LEN,
@@ -72,10 +81,11 @@ def run(theta):
     output['data_PS'] = data_PS
     output['z_PS'] = z_PS
     x = 1
-    with open('/gpfs0/elyk/users/hovavl/21CMPSemu/simulations/testFDM.pk',
+    # f_FDM = 10**(-f_FDM)
+    with open(f'/gpfs0/elyk/users/hovavl/jobs/FDM_jobs/psResults/21cmFAST_outputs_without_MCGs_example.pk',
               'wb') as f:
         pickle.dump(output, f)
 
-run([-1.22,-2.72, 0.51, -0.02, -1.03, -2.32, 0.11,  8.59, 39, 0.74])
+run([-1.25,-2.5, 0.5, 0.0, -1.35, -2.32, -0.3,  8.59, 40.5, 0.5])
 
 
